6d6b9c599c6904111ed78fc9871aa864e29b37d9
Agal2505 <agalya9865@gmail.com>
2023-10-18 17:55:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css'
+import EnrollComponent from './Component/EnrollComponent/EnrollComponent'
+
+function App() {
+  
+
+  return (
+    <EnrollComponent/>
+  )
+}
+
+export default App
---
+++ src/routes/LoginComponent/LoginComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoginComponent = () => {
+    
+  return (
+    <div>LoginComponent</div>
+  )
+}
+
+export default LoginComponent
